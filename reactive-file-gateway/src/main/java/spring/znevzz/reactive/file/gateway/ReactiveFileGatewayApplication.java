82a18ece07e5acf0490167f54ea0d58e216a97ee
ZNevzz <dsouza.nevil45@gmail.com>
2019-10-16 19:41:34 +0530

    subsribe not working as expected in file-gateway

--- reactive-file-gateway/src/main/java/spring/znevzz/reactive/file/gateway/ReactiveFileGatewayApplication.java
+++ reactive-file-gateway/src/main/java/spring/znevzz/reactive/file/gateway/ReactiveFileGatewayApplication.java
@@ -1,12 +1,16 @@
 package spring.znevzz.reactive.file.gateway;
 
-import org.springframework.beans.factory.annotation.Autowired;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Flux;
-import spring.znevzz.reactive.file.gateway.dao.FileRepository;
+import reactor.core.publisher.Mono;
+import spring.znevzz.reactive.file.gateway.dao.LocalFileRepository;
+
+import java.time.Duration;
+
 
 @SpringBootApplication
 public class ReactiveFileGatewayApplication {
@@ -17,20 +21,39 @@ public class ReactiveFileGatewayApplication {
 
 }
 
-
+@Slf4j
 @RestController
 class ReactiveFileGatewayController {
 
-	private final FileRepository repository;
+	private final LocalFileRepository repository;
 
-	ReactiveFileGatewayController(FileRepository repository) {
+	ReactiveFileGatewayController(LocalFileRepository repository) {
 		this.repository = repository;
 	}
 
 	@GetMapping(value = "/funds", produces = "application/stream+json")
 	public Flux<String> getFunds() {
-		return repository.readComplete(null)
-				.log();
+		Flux<String> stringFlux = repository.readComplete(null);
+
+		stringFlux.subscribe(this::info);
+
+		addFund();
+
+		return stringFlux
+//				.delayElements(Duration.ofSeconds(5))
+//				.log()
+			;
 	}
+
+	private void info(String s) {
+		log.info("value="+ s);
+	}
+
+	@GetMapping(value = "/add")
+	public Mono<Void> addFund() {
+		repository.setArray(null);
+		return Mono.empty();
+	}
+
 
 }
--- reactive-file-gateway/src/main/java/spring/znevzz/reactive/file/gateway/dao/LocalFileRepository.java
+++ reactive-file-gateway/src/main/java/spring/znevzz/reactive/file/gateway/dao/LocalFileRepository.java
@@ -5,6 +5,10 @@ import org.springframework.stereotype.Component;
 import reactor.core.publisher.Flux;
 import spring.znevzz.reactive.file.gateway.model.FileResource;
 
+import java.time.Duration;
+import java.util.ArrayList;
+import java.util.List;
+
 
 @Component
 public class LocalFileRepository implements FileRepository<String> {
@@ -12,9 +16,20 @@ public class LocalFileRepository implements FileRepository<String> {
     @Value("${sample.json.data}")
     private String data;
 
+    private List<String> array;
+    private Flux<String> flux;
+
+    public LocalFileRepository() {
+        this.array = new ArrayList<>();
+        this.flux = Flux.fromIterable(array);
+    }
+
     @Override
     public Flux<String> readComplete(FileResource fileResource) {
-        return Flux
-                .just(data);
+        return flux;
+    }
+
+    public void setArray(String element) {
+        array.add( element == null? data : element );
     }
 }
